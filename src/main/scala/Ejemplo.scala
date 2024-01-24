eb2749f9690bcb107fc6ecb6ec33c3083bd7e84e
rdiegoojeda24 <rdiegoojeda24@gmail.com>
2024-01-23 22:56:51 -0500

    Code 5

--- src/main/scala/Ejemplo.scala
+++ src/main/scala/Ejemplo.scala
@@ -10,31 +10,26 @@ import java.nio.file.Paths
 
 object Ejemplo extends App{
   implicit val actor: ActorSystem = ActorSystem("Ejemplo")
-  implicit val materializer: ActorMaterializer = ActorMaterializer()
+  implicit val mat: ActorMaterializer = ActorMaterializer()
   implicit val ec: ExecutionContext = actor.dispatcher
 
-  // Define la ruta del archivo de entrada y salida
-  val entrada = Paths.get("C://Users//usuario//OneDrive//Escritorio//Texto Minusculas.txt")
-  val salida = Paths.get("C://Users//usuario//OneDrive//Escritorio//Texto Mayusculas.txt")
+  val entrada = Paths.get("C:/Users/usuario/OneDrive/Escritorio/Texto Minusculas.txt")
+  val salida = Paths.get("C:/Users/usuario/OneDrive/Escritorio/Texto Mayusculas.txt")
 
-  // Define un flujo de Akka Streams para procesar el archivo
   val proceso: Flow[ByteString, ByteString, _] =
     Flow[ByteString]
       .via(Framing.delimiter(ByteString("\n"), maximumFrameLength = 256, allowTruncation = true))
       .map(line => transLinea(line.utf8String))
       .map(line => ByteString(line + "\n"))
 
-  // Lee el archivo de entrada, procesa las líneas y escribe el resultado en el archivo de salida
   val resultado = FileIO.fromPath(entrada)
     .via(proceso)
     .runWith(FileIO.toPath(salida))
 
-  // Maneja el resultado de la operación
   resultado.onComplete { _ =>
     actor.terminate()
   }
 
-  // Transforma una línea (simplemente convierte a mayúsculas en este caso)
   def transLinea(line: String): String = line.toUpperCase
 
 }
