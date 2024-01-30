a23aaa9e4295ab7b372674fc7bf928dde3ea7a7f
rdiegoojeda24 <rdiegoojeda24@gmail.com>
2024-01-30 15:42:21 -0500

    Code 6

--- src/main/scala/Ejemplo.scala
+++ src/main/scala/Ejemplo.scala
@@ -9,27 +9,29 @@ import scala.concurrent.ExecutionContext
 import java.nio.file.Paths
 
 object Ejemplo extends App{
-  implicit val actor: ActorSystem = ActorSystem("Ejemplo")
-  implicit val mat: ActorMaterializer = ActorMaterializer()
-  implicit val ec: ExecutionContext = actor.dispatcher
+  //Se crea los implicts para facilitar el uso
+  implicit val actor: ActorSystem = ActorSystem("Ejemplo")//Trabajar con actores
+  implicit val mat: ActorMaterializer = ActorMaterializer()//Necesario para trabajar con Akka Streams
+  implicit val ec: ExecutionContext = actor.dispatcher//Maneja operaciones asincronas
 
+  //Defino los archivos de entrada y salida
   val entrada = Paths.get("C:/Users/usuario/OneDrive/Escritorio/Texto Minusculas.txt")
   val salida = Paths.get("C:/Users/usuario/OneDrive/Escritorio/Texto Mayusculas.txt")
 
-  val proceso: Flow[ByteString, ByteString, _] =
+  val proceso: Flow[ByteString, ByteString, _] =//Se define flujo de Akka Streams
     Flow[ByteString]
-      .via(Framing.delimiter(ByteString("\n"), maximumFrameLength = 256, allowTruncation = true))
-      .map(line => transLinea(line.utf8String))
-      .map(line => ByteString(line + "\n"))
+      .via(Framing.delimiter(ByteString("\n"), maximumFrameLength = 256, allowTruncation = true))//divide las lineas
+      .map(line => transLinea(line.utf8String))//Aplica la funcion para transformar
+      .map(line => ByteString(line + "\n"))//Agrega un nuevo caracter a la linea
 
-  val resultado = FileIO.fromPath(entrada)
-    .via(proceso)
-    .runWith(FileIO.toPath(salida))
+  val resultado = FileIO.fromPath(entrada)//Lee el contenido de la entrada
+    .via(proceso)//Aplica el flujo
+    .runWith(FileIO.toPath(salida))//Escribe el resultado
 
-  resultado.onComplete { _ =>
-    actor.terminate()
+  resultado.onComplete { _ =>//Callback para manejar el resultado
+    actor.terminate()//Cierra el sistema de actores
   }
 
-  def transLinea(line: String): String = line.toUpperCase
+  def transLinea(line: String): String = line.toUpperCase//Funcion que toma una linea y la devuelve en MYS
 
 }
